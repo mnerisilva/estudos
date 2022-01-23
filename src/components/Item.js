37b3d1c398a5884583a7fea72bcb02be5c5a5db1
Marcelo Neri da Silva <marceloneri@gmail.com>
2022-01-23 01:42:10 -0300

    update

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,9 +2,10 @@ function Item({ list }) {
   return (
     <div>
       {list.map((item) => {
+        let string = item.tipo === 1 ? "" : " -";
         return (
           <li className={item.tipo === 1 ? "plus" : "minus"} key={item.id}>
-            {item.operacao} <span>R$ {item.valor}</span>
+            {item.operacao} <span>R$ {item.valor + string}</span>
             <button className="delete-btn">x</button>
           </li>
         );
